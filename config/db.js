971d6249e91bf803b454478d3185c82fc12cd473
LinHtetAung4 <alinhtet134@gmail.com>
2025-07-25 13:04:54 +0630

    first commit

---
+++ config/db.js
@@ -0,0 +1,27 @@
+import mongoose, { mongo } from "mongoose";
+import { cache } from "react";
+
+let cached = global.mongoose
+
+if (!cached){
+    cached=global.mongoose={ conn:null,promise:null
+
+    }
+}
+    async function connectDB(){
+        if(cached.conn){
+            return cached.conn
+        }
+        if(!cached.promise){
+            const opts ={
+                bufferCommands:false
+            }
+            cached.promise =mongoose.connect('${process.env.MONGODB_URI}/quickcart',opts).then(mongoose =>{
+                return mongoose
+            })
+        }
+        cached.com = await cached.promise
+        return cached.conn
+    }
+
+    export default  connectDB
